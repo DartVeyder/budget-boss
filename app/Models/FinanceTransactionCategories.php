280246242f0f9e7435bffbf98efb75f743753fd2
DartVeyder <medvsd.dmitro@gmail.com>
2024-05-05 13:45:15 +0300

    first commit

---
+++ app/Models/FinanceTransactionCategories.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FinanceTransactionCategories extends Model
+{
+    use HasFactory;
+}
